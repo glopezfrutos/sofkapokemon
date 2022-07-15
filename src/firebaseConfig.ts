de680ee588f3d37c83a2bfe888f6edd603994397
glopezfrutos <gus.lopezfrutos@gmail.com>
2022-07-15 01:07:04 -0300

    ADD - Firebase hosting config

---
+++ src/firebaseConfig.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCb89bmrMOcsQIGo6KCxPe0RGgS5QkFdO0",
+  authDomain: "pokemonapilist.firebaseapp.com",
+  projectId: "pokemonapilist",
+  storageBucket: "pokemonapilist.appspot.com",
+  messagingSenderId: "157372931959",
+  appId: "1:157372931959:web:73cf0a02983aa537f00b72"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
